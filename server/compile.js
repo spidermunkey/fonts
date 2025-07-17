53f5c5014d9dc7bf6c8bb37b81c33de6d89e5f21
spider <burkstremane@gmail.com>
2025-07-17 06:32:46 -0600

    update fs data

--- server/compile.js
+++ server/compile.js
@@ -3,9 +3,14 @@ const path = require('path');
 const {uuid} = require('./utils/uuid');
 const {countFiles} = require('./utils/readdirp');
 
-exports.compile_fonts = async function compile_fonts() {
+exports.compile_fonts = compile_fonts;
+
+async function compile_fonts() {
   const dir = "C:/Users/justi/dev/data/fonts";
   const data = {};
+  const name_index = {};
+  const id_index = {};
+
   const folders = await fs.readdir(dir,{withFileTypes:true});
   for (const item of folders){
     const name = item.name;
@@ -15,17 +20,27 @@ exports.compile_fonts = async function compile_fonts() {
 
     } else if (item.isDirectory()){
       // parse directory
-      console.log('parsing font directory', fontName);
+      const fontData = await parse_font_directory(path.join(dir,item.name));
+      for (const font in fontData){
+        name_index[fontData[font].name] = fontData[font]
+        id_index[fontData[font].id] = fontData[font]
+      }
       data[fontName] = {
         name: fontName,
         cid: uuid(),
         count: await countFiles(path.join(dir,item.name),['.ttf','.otf','.wof']),
-        fonts: await parse_font_directory(path.join(dir,item.name)),
+        fonts: fontData,
     }
   }
   }
-  console.log(data)
-  return data;
+  return {data,name_index,id_index};
+}
+
+async function overwrite(){
+  const {data,name_index,id_index} = await compile_fonts();
+  await fs.writeJSON(path.resolve('.','data/fonts.json'),data)
+  await fs.writeJSON(path.resolve('.','data/id_index.json'),id_index)
+  await fs.writeJSON(path.resolve('.','data/name_index.json'),name_index)
 }
 
 async function parse_font_directory(dir) {
@@ -38,19 +53,23 @@ async function parse_font_directory(dir) {
     const directory = await fs.readdir(dir,{withFileTypes:true});
     for (const item of directory){
       if (item.isFile() && isFont(item.name)){
-        console.log('font found!', item.name)
-        data[item.name.slice(0,-4)] = {
-          name: item.name.slice(0,-4).split(/[_\-]/).join(' '),
-          path:path.join(item.path,item.name),
-          type:item.name.slice(-3),
+        const fontName = item.name.slice(0,-4);
+        const id = uuid();
+        const font = {
+          id,
+          name: fontName,
+          path: path.join(item.path,item.name),
+          type: item.name.slice(-4),
           size: Math.round((await fs.stat(path.join(item.path,item.name))).size / 1000),
-          content: await fs.readFile(path.join(item.path,item.name)),
         };
+        data[fontName] = font;
       } else if (item.isDirectory()){
         readdir_recursive(path.join(dir,item.name))
       }
     }
   }
   await readdir_recursive(dir);
-  return data;
+  return data
 }
+
+overwrite();
